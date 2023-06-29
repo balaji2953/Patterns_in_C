d042c53a693a92bda3848f42dcb433e08e859110
balaji2953 <balaji290503@gmail.com>
2023-06-29 19:41:56 +0530

    Pattern_5

---
+++ 5_rightangle_triangle.c
@@ -0,0 +1,30 @@
+//Right angle triangle
+#include<stdio.h>
+int main()
+{
+    int n=10,i,j;
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/*
+output
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+* * * * * * *
+* * * * * * * *
+* * * * * * * * *
+* * * * * * * * * *
+
+*/
